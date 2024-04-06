fc017b578e21a09d120dcd5400ec2c9280f84711
Domclem02 <160752535+Domclem02@users.noreply.github.com>
2024-04-05 19:55:12 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
+// script.js
 document.getElementById("redirectButton").addEventListener("click", function() {
     // Replace 'https://example.com' with the URL you want to redirect to
-    window.location.href = 'https://example.com';
+    window.location.replace('https://example.com');
 });
