0d3d4e286f961880300317affe25c58da4b63a05
Domclem02 <160752535+Domclem02@users.noreply.github.com>
2024-04-05 20:03:47 -0500

    Adding buttons and functionality for buttons

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.getElementById("redirectButton").addEventListener("click", function() {
+document.getElementById("wordle").addEventListener("click", function() {
+    setTimeout(function() {
+        window.location.replace('https://www.nytimes.com/games/wordle/index.html');
+    }, 100); // 100 milliseconds delay
+});
+document.getElementById("connections").addEventListener("click", function() {
     // Replace 'https://example.com' with the URL you want to redirect to
     setTimeout(function() {
-        window.location.replace('https://example.com');
+        window.location.replace('https://www.nytimes.com/games/connections');
     }, 100); // 100 milliseconds delay
 });
